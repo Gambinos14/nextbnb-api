39504b12db74a2ace22ebda24ed80dd0a57be88b
Gambinos14 <gambinostefano14@gmail.com>
2020-06-25 14:53:23 -0400

    Deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn nextbnb-api.wsgi
+release: python manage.py migrate
--- nextbnb-api/settings.py
+++ nextbnb-api/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'nextbnb-api.urls'
@@ -154,5 +155,7 @@ USE_TZ = True
 # optional package: http://whitenoise.evans.io/en/stable/django.html
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'api.User'
